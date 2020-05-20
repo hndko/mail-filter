9e29f33d96f9e3823de0e3f9c66f42d3199ee71c
kyo1337 <kyuoko13@gmail.com>
2020-05-20 13:49:45 +0700

    Update mailfilter.py

--- mailfilter.py
+++ mailfilter.py
@@ -12,7 +12,6 @@ __  /|_/ /_  __ `/_  /__  / __  /_   __  /__  /_  __/  _ \_  ___/
 _  /  / / / /_/ /_  / _  /___  __/   _  / _  / / /_ /  __/  /
 /_/  /_/  \__,_/ /_/  /_____/_/      /_/  /_/  \__/ \___//_/
                                                        v.01
-%s
 [+] Author      : Ruitze
 [+] Tool        : MailFilter v.01
 [+] Description : Filter mailist Gmail, Yahoo, Hotmail
@@ -56,9 +55,9 @@ def main():
                             f.write(email + '\n')
                             f.close()
                     author()
-                    print('%s[+] Succes filter to gmailFile.txt   [+]%s' )
-                    print('%s[+] Succes filter to yahooFile.txt   [+]%s' )
-                    print('%s[+] Succes filter to hotmailFile.txt [+]%s' )
+                    print('[+] Succes filter to gmailFile.txt   [+]' )
+                    print('[+] Succes filter to yahooFile.txt   [+]' )
+                    print('[+] Succes filter to hotmailFile.txt [+]' )
                 else:
                     print('"{}" is not a file.').format(arg)
 
